a8d49973215cc9163161be51a7ead452b60926f1
MD.OLI ULLAH ANTOR <85104011+Antor-094@users.noreply.github.com>
2022-02-26 19:37:39 +0600

    Add sign in

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
-router.post('/',(req,res,next)=>{
+router.post('/signup',(req,res,next)=>{
     bcrypt.hash(req.body.password,10,(err,hash)=>{
         if(err)
         {
@@ -39,6 +39,51 @@ router.post('/',(req,res,next)=>{
             })
         }
     })
+})
+
+router.post('/signin',(req,res,next)=>{
+  User.find({username:req.body.username})
+  .exec()
+  .then(user=>{
+    if(user.length <1){
+      return res.status(401).json({
+        mes:'User not exist'
+      })
+    }
+    bcrypt.compare(req.body.password,user[0].password,(err,result)=>{
+      if(!result){
+        return res.status(401).json({
+          mes:'Password not matched'
+        })
+      }
+      if(result){
+        const token = jwt.sign({
+          username:user[0].username,
+          userType:user[0].userType,
+          email:user[0].email,
+          phone:user[0].phone
+        },
+
+        'this is secret token key',
+        {
+          expiresIn:"24h"
+        }
+        );
+        res.status(200).json({
+          username:user[0].username,
+          userType:user[0].userType,
+          email:user[0].email,
+          phone:user[0].phone,
+          token:token
+        })
+      }
+    })
+  })
+  .catch(err=>{
+    res.status(500).json({
+      error:err
+    })
+  })
 })
 
 module.exports= router;
